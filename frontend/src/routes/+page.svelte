b250adf2b388720ca0a64d9d0978836bb13505c1
Ferozaffs <zmass@ZMASS>
2023-07-25 20:59:56 +0200

    initial commit

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<div class="flex h-24 justify-center items-center bg-gray-800">
+    <p class="text-5xl text-gray-400">Markdown blog</p>
+</div>
+
+<div class="flex h-fit justify-center bg-gray-600">
+    <ul class="p-4 whitespace-nowrap no-scrollbar overflow-x-auto overflow-y-hidden">
+        <li class="px-2 min-w-24 w-fit inline-block">
+            <button class="min-w-24 text-gray-400 bg-gray-800 hover:bg-gray-900 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700">Blog</button>
+        </li>
+        <li class="px-2 w-fit inline-block">
+            <button class="min-w-24 text-gray-400 bg-gray-800 hover:bg-gray-900 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700">Projects</button>
+        </li>  
+        <li class="px-2 min-w-24 w-fit inline-block">
+            <button class="min-w-24 text-gray-400 bg-gray-800 hover:bg-gray-900 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700">About</button>
+        </li>
+    </ul>
+</div>
