6d1560bf94f18a0717edc0d8ecf8aac4bcfb62a0
Ferozaffs <zmass@ZMASS>
2023-07-25 21:23:31 +0200

    setup some basic components

---
+++ frontend/src/lib/Button.svelte
@@ -0,0 +1,5 @@
+<script>
+	export let text = 'Button';
+</script>
+
+<button class="min-w-24 text-gray-400 bg-gray-800 hover:bg-gray-900 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700">{text}</button>
---
+++ frontend/src/lib/ButtonRow.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Button from '$lib/Button.svelte';
+	export let buttons = ['Button 1', 'Button 2', 'Button 3'];
+</script>
+
+<ul class="p-4 whitespace-nowrap no-scrollbar overflow-x-auto overflow-y-hidden">
+    {#each buttons as buttonText}
+        <li class="px-2 min-w-24 w-fit inline-block">
+            <Button text={buttonText}/>
+        </li>
+    {/each}
+</ul>
