1d4de945e4efb05d326cdf4beddc7aa5271c07d8
Ferozaffs <zmass@ZMASS>
2023-10-16 21:20:53 +0200

    added markdown view and file load

--- admin_panel/src/main.rs
+++ admin_panel/src/main.rs
@@ -1,6 +1,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use std::fs;
+
 use eframe::egui;
+use egui_file::FileDialog;
+use egui_commonmark::{CommonMarkCache, CommonMarkViewer};
 
 fn main() -> Result<(), eframe::Error> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -22,12 +26,18 @@ fn main() -> Result<(), eframe::Error> {
 
 struct AdminPanel {
     markdown: String,
+    cache: CommonMarkCache,
+    opened_file: String,
+    open_file_dialog: Option<FileDialog>,
 }
 
 impl Default for AdminPanel {
     fn default() -> Self {
         Self {
-            markdown: "_".to_owned(),
+            markdown: "_".to_string(),
+            cache: CommonMarkCache::default(),
+            opened_file: "_".to_string(),
+            open_file_dialog: None
         }
     }
 }
@@ -37,21 +47,55 @@ impl eframe::App for AdminPanel {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
                 if ui.button("Load").clicked() {
-                    self.markdown = "Load".to_owned();
+                    let mut dialog = FileDialog::open_file(None);
+                    dialog.open();
+                    self.open_file_dialog = Some(dialog);
                 }
                 else if ui.button("Save").clicked() {
-                    self.markdown = "Save".to_owned();
+                    fs::write(self.opened_file.clone() + "_new", self.markdown.clone()).expect("Unable to write file");
                 }
                 else if ui.button("Upload").clicked() {
                     self.markdown = "Upload".to_owned();
                 }
                 
             });
+
+            if let Some(dialog) = &mut self.open_file_dialog {
+                if dialog.show(ctx).selected() {
+                    if let Some(file) = dialog.path() {
+                        self.opened_file = Some(file.to_path_buf()).unwrap().to_str().unwrap().to_string();
+                        self.markdown = fs::read_to_string(self.opened_file.clone()).unwrap();  
+                    }
+                }
+            }     
+
             let width = ui.available_width();
             let height = ui.available_height();
 
             ui.horizontal(|ui| {
                 ui.set_min_height(height);
+                egui::ScrollArea::vertical()
+                    .id_source("data")
+                    .show(ui, |ui| {
+                        ui.vertical(|ui| {
+                            ui.add_sized(
+                                [width * 0.2, height * 0.1],
+                                egui::Button::new("Test1")
+                            );
+                            ui.add_sized(
+                                [width * 0.2, height * 0.1],
+                                egui::Button::new("Test2")
+                            );
+                            ui.add_sized(
+                                [width * 0.2, height * 0.1],
+                                egui::Button::new("Test3")
+                            );
+                            ui.add_sized(
+                                [width * 0.2, height * 0.1],
+                                egui::Button::new("Test4")
+                            );
+                        });
+                    });
                 egui::ScrollArea::vertical()
                     .id_source("source")
                     .show(ui, |ui| {
@@ -63,37 +107,11 @@ impl eframe::App for AdminPanel {
                 egui::ScrollArea::vertical()
                     .id_source("preview")
                     .show(ui, |ui| {
-                        ui.add_sized(
-                            [width * 0.4, height],
-                            egui::Label::new(self.markdown.to_owned()),
-                        );
-                    });
-                egui::ScrollArea::vertical()
-                    .id_source("data")
-                    .show(ui, |ui| {
-                        ui.add_sized(
-                            [width * 0.2, height],
-                            egui::Label::new(self.markdown.to_owned()),
-                        );
+                        ui.with_layout(egui::Layout::top_down(eframe::emath::Align::Min), |ui| {
+                            CommonMarkViewer::new("viewer").show(ui, &mut self.cache, &self.markdown);
+                        });
                     });
             });
-            //ui.columns(3, |columns|{
-            //    egui::ScrollArea::vertical()
-            //        .id_source("source")
-            //        .show(&mut columns[0], |ui| {
-            //            ui.text_edit_multiline(&mut self.text);
-            //        });
-            //    egui::ScrollArea::vertical()
-            //        .id_source("preview")
-            //        .show(&mut columns[1], |ui| {
-            //            ui.label(format!("{}", self.text));
-            //        });
-            //    egui::ScrollArea::vertical()
-            //        .id_source("data")
-            //        .show(&mut columns[2], |ui| {
-            //            ui.label(format!("Data"));
-            //        });     
-            //});
        });
     }
 }
