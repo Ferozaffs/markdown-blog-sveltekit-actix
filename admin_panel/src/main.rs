3befee892113ef1fdcb197d98d53693f1b1d21fa
root <root@ZMASS>
2024-06-17 20:29:56 +0200

    fixed meta data handling

--- admin_panel/src/main.rs
+++ admin_panel/src/main.rs
@@ -3,8 +3,9 @@
 use std::fs;
 
 use eframe::egui;
-use egui_file::FileDialog;
 use egui_commonmark::{CommonMarkCache, CommonMarkViewer};
+use egui_file::FileDialog;
+use regex::Regex;
 
 fn main() -> Result<(), eframe::Error> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -26,12 +27,12 @@ fn main() -> Result<(), eframe::Error> {
 
 enum FileLoadState {
     LoadMarkdown,
-    LoadImage
+    LoadImage,
 }
 
 struct MetaData {
     title: String,
-    tags: Vec<String>
+    tags: Vec<String>,
 }
 
 struct AdminPanel {
@@ -54,7 +55,10 @@ impl Default for AdminPanel {
             opened_markdown_file: "".to_string(),
             file_load_state: None,
             open_file_dialog: None,
-            meta_data: None,
+            meta_data: Some(MetaData {
+                title: String::from(""),
+                tags: vec![],
+            }),
             tag_field: "".to_string(),
         }
     }
@@ -62,27 +66,22 @@ impl Default for AdminPanel {
 
 impl eframe::App for AdminPanel {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        
         self.check_for_macros();
         self.file_dialog_handler(ctx);
-        
+
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
                 if ui.button("Load").clicked() {
                     self.file_load_state = Some(FileLoadState::LoadMarkdown);
-                }
-                else if ui.button("Save").clicked() {
-                    let meta_data = self.markdown.find("@@META");
-                    if meta_data.is_none() {
-                        self.genereate_meta_data();
-                    }
-
-                    fs::write(self.opened_markdown_file.clone() + "_new", self.markdown.clone()).expect("Unable to write file");
-                }
-                else if ui.button("Upload").clicked() {
+                } else if ui.button("Save").clicked() {
+                    fs::write(
+                        self.opened_markdown_file.clone() + "_new",
+                        self.markdown.clone(),
+                    )
+                    .expect("Unable to write file");
+                } else if ui.button("Upload").clicked() {
                     self.markdown = "Upload".to_owned();
                 }
-                
             });
 
             let width = ui.available_width();
@@ -95,16 +94,14 @@ impl eframe::App for AdminPanel {
                     .id_source("data")
                     .show(ui, |ui| {
                         ui.vertical(|ui| {
-                            ui.add_sized(
-                                [width * 0.2, height * 0.1],
-                                egui::Label::new("Options")
-                            );
+                            ui.add_sized([width * 0.2, height * 0.1], egui::Label::new("Options"));
 
                             let mut regenerate = false;
-                            if let Some(meta_data) = self.meta_data.as_mut() {                              
+                            if let Some(meta_data) = self.meta_data.as_mut() {
                                 ui.label("Title");
 
-                                let response = ui.add(egui::TextEdit::singleline(&mut meta_data.title));
+                                let response =
+                                    ui.add(egui::TextEdit::singleline(&mut meta_data.title));
                                 if response.changed() {
                                     regenerate = true;
                                 }
@@ -114,12 +111,16 @@ impl eframe::App for AdminPanel {
                                     ui.add(egui::TextEdit::singleline(&mut self.tag_field));
                                     let response = ui.button("Add");
                                     if response.clicked() {
-                                        meta_data.tags.push(self.tag_field.clone());
-                                        regenerate = true;
+                                        if self.tag_field.len() > 0
+                                            && !meta_data.tags.contains(&self.tag_field)
+                                        {
+                                            meta_data.tags.push(self.tag_field.clone());
+                                            regenerate = true;
+                                        }
                                     }
                                 });
 
-                                let mut index_removal: Vec<usize> = Vec::new(); 
+                                let mut index_removal: Vec<usize> = Vec::new();
                                 for (i, tag) in meta_data.tags.iter().enumerate() {
                                     ui.horizontal(|ui| {
                                         ui.label(tag);
@@ -127,17 +128,16 @@ impl eframe::App for AdminPanel {
                                         if response.clicked() {
                                             index_removal.push(i)
                                         }
-                                    });                                  
+                                    });
                                 }
 
-                                if index_removal.len() > 0 {                                       
+                                if index_removal.len() > 0 {
                                     for index in index_removal.iter() {
                                         meta_data.tags.remove(*index);
                                     }
                                     regenerate = true
                                 }
-                            }
-                            else {
+                            } else {
                                 ui.label("No data");
                             }
 
@@ -160,29 +160,21 @@ impl eframe::App for AdminPanel {
                     .id_source("preview")
                     .show(ui, |ui| {
                         ui.with_layout(egui::Layout::top_down(eframe::emath::Align::Min), |ui| {
-                            CommonMarkViewer::new("viewer").show(ui, &mut self.cache, &self.markdown);
+                            CommonMarkViewer::new("viewer").show(
+                                ui,
+                                &mut self.cache,
+                                &self.markdown,
+                            );
                         });
                     });
             });
-       });
+        });
     }
 }
 
 impl AdminPanel {
-    fn genereate_meta_data(&mut self) {
-        self.meta_data = Some(MetaData{
-            title: String::from("Title"),
-            tags: vec![
-                String::from("Tag1"),
-                String::from("Tag2")
-             ]
-        });
-
-        self.regenereate_meta_data(false);
-    }
-
     fn regenereate_meta_data(&mut self, clean: bool) {
-        let mut md_meta: String = "@@META\n".to_string();
+        let mut md_meta: String = "@META\n".to_string();
         md_meta.push_str(format!("@TITLE: {}\n", self.meta_data.as_ref().unwrap().title).as_str());
         md_meta.push_str("@TAGS: ");
         for tag in self.meta_data.as_ref().unwrap().tags.iter() {
@@ -198,17 +190,40 @@ impl AdminPanel {
         self.markdown = format!("{}\n\n{}", md_meta, self.markdown);
     }
 
-    fn load_meta_data(&mut self){
+    fn load_meta_data(&mut self, clean: bool) {
+        if let Some(meta_data) = self.meta_data.as_mut() {
+            let re = Regex::new(r"@TITLE:\s(.*)").unwrap();
+            if let Some(caps) = re.captures(self.markdown.as_str()) {
+                meta_data.title = caps[1].to_string();
+            } else {
+                let re = Regex::new(r"#\s(.*)").unwrap();
+                if let Some(caps) = re.captures(self.markdown.as_str()) {
+                    meta_data.title = caps[1].to_string();
+                }
+            }
 
+            meta_data.tags.clear();
+            let re = Regex::new(r"@TAGS:\s(.*)").unwrap();
+            if let Some(caps) = re.captures(self.markdown.as_str()) {
+                let tags = caps[1].split(" ");
+                for tag in tags {
+                    if tag.len() > 0 {
+                        meta_data.tags.push(tag.to_string());
+                    }
+                }
+            }
+
+            self.regenereate_meta_data(clean);
+        }
     }
 
     fn check_for_macros(&mut self) {
         if self.prev_markdown != self.markdown {
             let meta_data = self.markdown.find("@@META");
             if meta_data.is_some() {
-                self.load_meta_data();
+                self.load_meta_data(false);
             }
-            
+
             let image = self.markdown.find("@@IMAGE");
             if image.is_some() {
                 self.file_load_state = Some(FileLoadState::LoadImage);
@@ -217,9 +232,8 @@ impl AdminPanel {
             self.prev_markdown = self.markdown.clone();
         }
     }
-    
-    fn file_dialog_handler(&mut self, ctx: &egui::Context) {   
 
+    fn file_dialog_handler(&mut self, ctx: &egui::Context) {
         if self.file_load_state.is_some() && self.open_file_dialog.is_none() {
             let mut dialog = FileDialog::open_file(None);
             dialog.open();
@@ -231,20 +245,30 @@ impl AdminPanel {
                 if let Some(file) = dialog.path() {
                     match self.file_load_state.as_ref().unwrap() {
                         FileLoadState::LoadMarkdown => {
-                            self.opened_markdown_file = Some(file.to_path_buf()).unwrap().to_str().unwrap().to_string();
-                            self.markdown = fs::read_to_string(self.opened_markdown_file.clone()).unwrap();                         
+                            self.opened_markdown_file = Some(file.to_path_buf())
+                                .unwrap()
+                                .to_str()
+                                .unwrap()
+                                .to_string();
+                            self.markdown =
+                                fs::read_to_string(self.opened_markdown_file.clone()).unwrap();
+
+                            let meta_data = self.markdown.find("@META");
+                            self.load_meta_data(meta_data.is_some())
                         }
                         FileLoadState::LoadImage => {
-                            let image_md = format!("![@IMAGE]({})", Some(file.to_path_buf()).unwrap().to_str().unwrap());
+                            let image_md = format!(
+                                "![@IMAGE]({})",
+                                Some(file.to_path_buf()).unwrap().to_str().unwrap()
+                            );
                             self.markdown = self.markdown.replace("@@IMAGE", &image_md);
                         }
-                    } 
+                    }
 
                     self.file_load_state = None;
                     self.open_file_dialog = None;
                 }
             }
-        }     
+        }
     }
 }
-
