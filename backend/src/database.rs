b7819ec781166700cfdb724b0df041025c15daf9
root <feroza_ffs@hotmail.com>
2024-09-09 20:28:32 +0200

    fixed sql insert of posts

--- backend/src/database.rs
+++ backend/src/database.rs
@@ -1,6 +1,8 @@
 use bb8::Pool;
 use bb8_postgres::PostgresConnectionManager;
-use tokio_postgres::{Row, NoTls, Error};
+use chrono::{DateTime, Utc};
+use tokio_postgres::{Error, NoTls, Row};
+use uuid::Uuid;
 
 type ConnectionPool = Pool<PostgresConnectionManager<NoTls>>;
 
@@ -11,7 +13,7 @@ pub struct Database {
 impl Database {
     pub async fn new() -> Self {
         let database_url = "postgresql://mdAdmin:1337asdf@db:5432/mdDatabase";
-        
+
         let manager = PostgresConnectionManager::new_from_stringlike(database_url, NoTls)
             .expect("Failed to create connection manager");
 
@@ -24,65 +26,79 @@ impl Database {
     }
 
     pub async fn get_project_categories(&self) -> Result<Vec<Row>, Error> {
-        let connection = self.pool.get()
-        .await
-        .expect("Failed to get a connection from the pool");
+        let connection = self
+            .pool
+            .get()
+            .await
+            .expect("Failed to get a connection from the pool");
 
         connection.query("SELECT project_categories.category, project_categories.description FROM project_categories",&[])
         .await
     }
 
     pub async fn get_projects_from_category(&self, category: &str) -> Result<Vec<Row>, Error> {
-        let connection = self.pool.get()
-        .await
-        .expect("Failed to get a connection from the pool");
+        let connection = self
+            .pool
+            .get()
+            .await
+            .expect("Failed to get a connection from the pool");
 
-        let query = format!("SELECT projects.id, projects.name, projects.image, projects.status
+        let query = format!(
+            "SELECT projects.id, projects.name, projects.image, projects.status
             FROM projects
             WHERE projects.category_id=(
                 SELECT project_categories.id 
                 FROM project_categories
                 WHERE project_categories.category='{}')",
-        category);
+            category
+        );
 
-        connection.query(&query.to_string() ,&[])
-        .await
+        connection.query(&query.to_string(), &[]).await
     }
 
     pub async fn get_project_summary(&self, id: &str) -> Result<Vec<Row>, Error> {
-        let connection = self.pool.get()
-        .await
-        .expect("Failed to get a connection from the pool");
+        let connection = self
+            .pool
+            .get()
+            .await
+            .expect("Failed to get a connection from the pool");
 
-        let query = format!("SELECT projects.id, projects.name, projects.image, projects.status
+        let query = format!(
+            "SELECT projects.id, projects.name, projects.image, projects.status
             FROM projects
             WHERE projects.id='{}'",
-        id);
+            id
+        );
 
-        connection.query(&query.to_string(), &[])
-        .await
+        connection.query(&query.to_string(), &[]).await
     }
 
     pub async fn get_project_content(&self, id: &str) -> Result<Vec<Row>, Error> {
-        let connection = self.pool.get()
-        .await
-        .expect("Failed to get a connection from the pool");
+        let connection = self
+            .pool
+            .get()
+            .await
+            .expect("Failed to get a connection from the pool");
 
-        let query = format!("SELECT projects.content 
+        let query = format!(
+            "SELECT projects.content 
             FROM projects 
             WHERE projects.id='{}'",
-        id);
+            id
+        );
 
-        connection.query(&query.to_string(),&[])
-        .await
+        connection.query(&query.to_string(), &[]).await
     }
 
     pub async fn get_posts(&self, tags: Vec<&str>) -> Result<Vec<Row>, Error> {
-        let connection = self.pool.get()
-        .await
-        .expect("Failed to get a connection from the pool");
+        let connection = self
+            .pool
+            .get()
+            .await
+            .expect("Failed to get a connection from the pool");
 
-        let mut query = format!("SELECT 
+        let mut query = format!(
+            "SELECT 
             posts.id, 
             posts.name, 
             posts.image, 
@@ -90,7 +106,8 @@ impl Database {
             posts.description,
             posts.tags,
             posts.project_id
-            FROM posts");
+            FROM posts"
+        );
 
         if tags.is_empty() == false {
             query = format!("{} WHERE", query);
@@ -98,25 +115,26 @@ impl Database {
             for (i, tag) in tags.iter().enumerate() {
                 if i == 0 {
                     query = format!("{} posts.tags LIKE \'%{}%\'", query, tag);
-                }
-                else {
+                } else {
                     query = format!("{} OR posts.tags LIKE \'%{}%\'", query, tag);
                 }
             }
         }
 
         query = format!("{} ORDER BY posts.date DESC", query);
-           
-        connection.query(&query.to_string(),&[])
-        .await
+
+        connection.query(&query.to_string(), &[]).await
     }
 
     pub async fn get_post_summary(&self, id: &str) -> Result<Vec<Row>, Error> {
-        let connection = self.pool.get()
-        .await
-        .expect("Failed to get a connection from the pool");
+        let connection = self
+            .pool
+            .get()
+            .await
+            .expect("Failed to get a connection from the pool");
 
-        let query = format!("SELECT 
+        let query = format!(
+            "SELECT 
             posts.id, 
             posts.name, 
             posts.image, 
@@ -126,23 +144,62 @@ impl Database {
             posts.project_id
             FROM posts
             WHERE posts.id='{}'",
-        id);
+            id
+        );
 
-        connection.query(&query.to_string(), &[])
-        .await
+        connection.query(&query.to_string(), &[]).await
     }
 
     pub async fn get_post_content(&self, id: &str) -> Result<Vec<Row>, Error> {
-        let connection = self.pool.get()
-        .await
-        .expect("Failed to get a connection from the pool");
+        let connection = self
+            .pool
+            .get()
+            .await
+            .expect("Failed to get a connection from the pool");
 
-        let query = format!("SELECT posts.content 
+        let query = format!(
+            "SELECT posts.content 
             FROM posts 
             WHERE posts.id='{}'",
-        id);
+            id
+        );
 
-        connection.query(&query.to_string(),&[])
-        .await
+        connection.query(&query.to_string(), &[]).await
+    }
+
+    pub async fn save_post(
+        &self,
+        title: &str,
+        tags: &str,
+        markdown: &str,
+        image: &str,
+    ) -> Result<u64, Error> {
+        let connection = self
+            .pool
+            .get()
+            .await
+            .expect("Failed to get a connection from the pool");
+
+        let query = format!(
+            "INSERT INTO posts (id, name, image, project_id, tags, content, date, description)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)"
+        );
+
+        let now: DateTime<Utc> = Utc::now();
+        connection
+            .execute(
+                &query,
+                &[
+                    &Uuid::new_v4(),
+                    &title,
+                    &image,
+                    &Uuid::nil(),
+                    &tags,
+                    &markdown,
+                    &now.naive_utc().date(),
+                    &"Temp description",
+                ],
+            )
+            .await
     }
 }
--- backend/src/uploads.rs
+++ backend/src/uploads.rs
@@ -1,10 +1,13 @@
 use crate::database::Database;
 use actix_web::{post, web, HttpResponse, Responder};
+use base64::prelude::*;
 use lazy_static::lazy_static;
 use nanoid::nanoid;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
+use std::fs::File;
+use std::io::Write;
 use std::sync::Mutex;
 
 #[derive(Deserialize)]
@@ -38,8 +41,8 @@ struct ImageResponse {
 }
 
 lazy_static! {
-    static ref FINGERPRINTS: Mutex<HashSet<String>> = {
-        let mut fingerprints = HashSet::new();
+    static ref FINGERPRINTS: Mutex<Vec<ImageUploadData>> = {
+        let fingerprints = Vec::new();
         Mutex::new(fingerprints)
     };
 }
@@ -58,41 +61,60 @@ pub async fn upload_post(db: web::Data<Database>, mut data: web::Json<PostData>)
         .into_iter()
         .collect();
 
+    remove_meta(&mut data.markdown);
     for p in unique_paths {
         let id = nanoid!();
 
         replace_path(&mut data.markdown, &p, &id);
 
-        let mut fp = FINGERPRINTS.lock().unwrap();
-        fp.insert(id.clone());
-
-        status.imagerequest.push(ImageUploadData {
+        let image_data = ImageUploadData {
             image_url: p,
             image_fingerprint: id,
-        });
+        };
+
+        let mut fp = FINGERPRINTS.lock().unwrap();
+        fp.push(image_data.clone());
+
+        status.imagerequest.push(image_data);
     }
 
-    //tokio::spawn(async move {
-    //    save_post(data.into_inner()).await;
-    //});
+    let mut id = "undefined".to_string();
+    if !status.imagerequest.is_empty() {
+        id = status
+            .imagerequest
+            .first()
+            .unwrap()
+            .image_fingerprint
+            .clone();
+    }
+
+    tokio::spawn(async move {
+        save_post(db, data.into_inner(), id).await;
+    });
 
     HttpResponse::Ok().json(status)
 }
 
 #[post("/upload_image")]
-pub async fn upload_image(db: web::Data<Database>, data: web::Json<ImageData>) -> impl Responder {
+pub async fn upload_image(_db: web::Data<Database>, data: web::Json<ImageData>) -> impl Responder {
     let mut status = ImageResponse {
         status: "Success".to_string(),
     };
 
     let mut fp = FINGERPRINTS.lock().unwrap();
-    if !fp.contains(&data.fingerprint) {
-        status.status = "Failed".to_string()
+
+    if let Some(index) = fp
+        .iter()
+        .position(|d| d.image_fingerprint == data.fingerprint)
+    {
+        let filename = extract_filename(fp[index].image_url.as_str());
+        fp.remove(index);
+
+        tokio::spawn(async move {
+            save_image(data.into_inner(), &filename).await;
+        });
     } else {
-        fp.remove(&data.fingerprint);
-        //tokio::spawn(async move {
-        //    save_image(data.into_inner()).await;
-        //});
+        status.status = "Failed".to_string()
     }
 
     HttpResponse::Ok().json(status)
@@ -125,4 +147,43 @@ fn replace_path(text: &mut String, path: &String, id: &String) {
 
     text.clear();
     text.push_str(result.as_str());
+}
+
+fn remove_meta(text: &mut String) {
+    let lines = text.lines().skip(4);
+    let remaining_text = lines.collect::<Vec<&str>>().join("\n");
+
+    text.clear();
+    text.push_str(remaining_text.as_str());
+}
+
+async fn save_post(db: web::Data<Database>, data: PostData, image_fingerprint: String) {
+    let result = db
+        .save_post(
+            data.title.as_str(),
+            data.tags.as_str(),
+            data.markdown.as_str(),
+            image_fingerprint.as_str(),
+        )
+        .await;
+
+    match result {
+        Ok(value) => println!("Uploaded post: {}", value),
+        Err(e) => println!("Error uploading post: {}", e),
+    }
+}
+
+async fn save_image(data: ImageData, filename: &str) {
+    let image_data = BASE64_STANDARD
+        .decode(&data.image)
+        .map_err(|e| actix_web::error::ErrorInternalServerError(e));
+
+    let file = File::create(format!("./assets/{}", filename))
+        .map_err(|e| actix_web::error::ErrorInternalServerError(e));
+
+    if file.is_ok() && image_data.is_ok() {
+        file.unwrap()
+            .write_all(&image_data.unwrap())
+            .map_err(|e| actix_web::error::ErrorInternalServerError(e));
+    }
 }
